7673a09573f5ef7b70d70e59cf6bbd4c546bfde1
Administrator <Administrator@DESKTOP-ITO9GJ1>
2025-06-03 11:50:08 +0300

    Add project files.

---
+++ Abstraction/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+
+        public abstract void Work();
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello, my name is {Name} {LastName} and I work in the {Department} department.");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} {LastName} is managing the team.");
+        }
+    }
+    public class Developer : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} {LastName} is developing software.");
+        }
+    }
+    public class Designer : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} {LastName} is designing user interfaces.");
+        }
+    }
+
+    public class  SalesOfficer : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} {LastName} is selling products.");
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,38 @@
+﻿using Abstraction;
+
+Employee employee1 = new Manager 
+{ 
+    Name = "Ajda", 
+    LastName = "Pekkan", 
+    Department = "Management" 
+};
+
+Employee employee2 = new Developer
+{
+    Name = "Cem",
+    LastName = "Yılmaz",
+    Department = "Development"
+};
+
+Employee employee3 = new Designer
+{
+    Name = "Banu",
+    LastName = "Alkan",
+    Department = "Design"
+};
+
+Employee employee4 = new SalesOfficer
+{
+    Name = "Hadise",
+    LastName = "Dümtektek",
+    Department = "Sales"
+};
+
+employee1.Introduce();
+employee1.Work();
+employee2.Introduce();
+employee2.Work();
+employee3.Introduce();
+employee3.Work();
+employee4.Introduce();
+employee4.Work();
